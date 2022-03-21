2d0857e936fc4827bd510894f63fd57c381fc51f
michaelcutro <mikercutro@gmail.com>
2022-03-21 00:50:21 -0400

    change bat file to CD omfg

--- serve_hw3.bat
+++ serve_hw3.bat
@@ -1,4 +1,4 @@
-C:\Users\vcm\PycharmProjects\homework_3-1
+cd C:\Users\vcm\PycharmProjects\homework_3-1
 git pull https://%TESTAPP_GIT_PAT%@github.com/MichaelCutro/homework_3-1.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
